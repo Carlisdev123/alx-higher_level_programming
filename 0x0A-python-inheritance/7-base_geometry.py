2913d0d916c06ac223bbc2bd7a532a3b94713736
Joseph Carlis Coleman <102561546+Carlisdev123@users.noreply.github.com>
2023-04-28 01:53:02 +0000

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""BaseGeometry module.
-Contains a class BaseGeometry, and some methods.
+"""
+===================================
+module with class BaseGeometry
+===================================
 """
 
 
-class BaseGeometry():
-    """Defines the BaseGeometry class."""
+class BaseGeometry:
+    """BaseGeometry class"""
 
     def area(self):
-        """Raises an Exception."""
+        """method for calculated area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value."""
+        """Method for validate if a num is integer"""
+
         if type(value) is not int:
-            raise TypeError(name + " must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
