8cf5471ab97768ee3012bda7c1406acb6a64d85a
Joseph Carlis Coleman <102561546+Carlisdev123@users.noreply.github.com>
2023-04-28 01:53:57 +0000

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""Rectangle module.
-Contains a class Rectangle that inherits from
-BaseGeometry and some methods.
-"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
 
 
 class Rectangle(BaseGeometry):
-    """Defines the Rectangle class that inherits from BaseGeometry."""
+    """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-        """Checks and sets the default attributes of Rectangle class."""
+        """Method for initialized the attrubutes"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
-    def __str__(self):
-        """Sets the str behaviour."""
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-
     def area(self):
-        """Returns the area of the rectangle."""
-        return self.__width * self.
+        """Method to redefine a area method in the parent class"""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """__str__ method for return the next string"""
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
